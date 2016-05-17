236ad81f3f1ac661412569740a2442612748cc86
luoweis <luoweis@126.com>
2016-05-17 23:17:57 +0800

    16年5月17日晚

--- flask/test_pro1/plugin.sh
+++ flask/test_pro1/plugin.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
 venv/bin/pip install flask
-venv/bin/pip install flask-login
-venv/bin/pip install flask-openid
-venv/bin/pip install flask-mail
-venv/bin/pip install flask-sqlalchemy
-venv/bin/pip install sqlalchemy-migrate
-venv/bin/pip install flask-whooshalchemy
 venv/bin/pip install flask-wtf
-venv/bin/pip install flask-babel
-venv/bin/pip install guess_language
-venv/bin/pip install flipflop
-venv/bin/pip install coverage
+pip install flask-script
+pip install flask-bootstrap
+pip install redis
+#mkdir app
+#mkdir app/static
+#mkdir app/templates
+#mkdir tmp
 
-
-mkdir app
-mkdir app/static
-mkdir app/templates
-mkdir tmp
-
-touch app/__init__.py
+#touch app/__init__.py
